274b0ea11a79b7c514d2f4ea49d2a3b297128619
Oluwasegun Ige <40037490+Tshergzeh@users.noreply.github.com>
2022-05-09 13:20:58 +0100

    Added a personalized welcome message

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,27 @@ myImage.onclick = function() {
         myImage.setAttribute('src', 'images/dp.jpg');
     }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Hi there, ' + myName;
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Hey there, ' + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+}
